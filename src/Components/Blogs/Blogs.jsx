77cf6fd5dd125f223d40e28bf9ab53a06700be6a
MirazZim <miraz.zim.38@gmail.com>
2025-01-05 12:46:59 +0600

    displayed Blog data and hashtags

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,6 +1,6 @@
-import { data } from "autoprefixer";
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
@@ -21,6 +21,14 @@ useEffect( () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs :{blogs.length}</h1>
+            {
+                blogs.map(blog =>  
+                <Blog 
+                key={blog.id} 
+                blog={blog}>
+                    
+                </Blog>)
+            }
         </div>
     );
 };
